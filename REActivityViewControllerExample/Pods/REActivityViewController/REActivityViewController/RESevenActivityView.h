ad41728b66a83e7dc8723f534cc961d83fb02083
Aleksandr Kardakov <razielsun@gmail.com>
2013-11-06 14:04:45 +0400

    some design fix

---
+++ REActivityViewControllerExample/Pods/REActivityViewController/REActivityViewController/RESevenActivityView.h
@@ -0,0 +1,13 @@
+//
+//  RESevenActivityView.h
+//  Pods
+//
+//  Created by Aleksandr Kardakov on 05.11.13.
+//
+//
+
+#import "REActivityView.h"
+
+@interface RESevenActivityView : REActivityView
+
+@end
